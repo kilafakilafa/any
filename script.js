921c7019fdf59d8c80b0fbd05765112b58d46e38
Uki Aula Fauzi <ukiaula39@gmail.com>
2023-10-23 10:16:29 +0700

    commit

--- script.js
+++ script.js
@@ -147,8 +147,8 @@ function initengahan(){
   document.getElementById("lv2").onclick = function() {lv2.style="opacity:0";slov+=1;this.onclick=null;checkslov();}
   document.getElementById("lv3").onclick = function() {lv3.style="opacity:0";slov+=1;this.onclick=null;checkslov();}
   document.getElementById("lv4").onclick = function() {lv4.style="opacity:0";slov+=1;this.onclick=null;checkslov();}
-  document.getElementById("lv3").onclick = function() {lv3.style="opacity:0";slov+=1;this.onclick=null;checkslov();}
-  document.getElementById("lv4").onclick = function() {lv4.style="opacity:0";slov+=1;this.onclick=null;checkslov();}
+  document.getElementById("lv5").onclick = function() {lv5.style="opacity:0";slov+=1;this.onclick=null;checkslov();}
+  document.getElementById("lv6").onclick = function() {lv6.style="opacity:0";slov+=1;this.onclick=null;checkslov();}
 
   var slov=0;function checkslov() {if(slov==6){kolombaru.style="position:relative;transform:scale(1)";otomatis();setTimeout(aktipesan2,400);}}
   
